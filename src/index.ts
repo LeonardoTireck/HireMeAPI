610ee735026c0f4d2b3dc025846292fad51566fb
Leonardo Tireck <leonardotireck@Leonardos-MacBook-Air.local>
2025-03-16 18:51:30 -0300

    eslint and prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import Server from "./server";
+import Server from './server';
 
 export class JobApplication {
-    run(){
-        const server = new Server();
-        server.listenServer();
-    }
+  run() {
+    const server = new Server();
+    server.listenServer();
+  }
 }
 
 const job = new JobApplication();
