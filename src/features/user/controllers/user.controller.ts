4171c4b01627a4748210ed08ec694d8d17a01ab9
Leonardo Tireck <leonardotireck@Leonardos-MacBook-Air.local>
2025-03-19 21:10:23 -0300

    add db connection, user migration table and json parser

--- src/features/user/controllers/user.controller.ts
+++ src/features/user/controllers/user.controller.ts
@@ -1,10 +1,31 @@
+import { User } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
+import prisma from '../../../globals/prisma';
 
 class UserController {
-  getAll(req: Request, res: Response, next: NextFunction) {
+  async getAll(req: Request, res: Response, next: NextFunction) {
+    const users = await prisma.user.findMany();
     res.status(200).json({
       message: 'users GET successfully',
-      data: [],
+      data: users,
+    });
+  }
+
+  async createUser(req: Request, res: Response, next: NextFunction) {
+    const { name, email, password, role } = req.body;
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+        role,
+      },
+    });
+
+    res.status(201).json({
+      message: 'User created',
+      data: user,
     });
   }
 }
--- src/features/user/routes/user.routes.ts
+++ src/features/user/routes/user.routes.ts
@@ -4,5 +4,6 @@ import userController from '../controllers/user.controller';
 const userRoutes = express.Router();
 
 userRoutes.get('/', userController.getAll);
+userRoutes.post('/create', userController.createUser);
 
 export default userRoutes;
